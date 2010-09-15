29f0cbbb54cae7e769aa3b9e0fa64a2474183754
lfborjas <luis.borjas@escolarea.com>
2010-09-15 10:09:26 -0600

    Initial commit of unmodified new Rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ricercar::Application.config.secret_token = '798c443d76991ed0f80bd99f49226fb48b3a7283f95321500c6d94a3818f78be78990491fea19a314da1d5cac32548909687cd3dd5193b988f799a8342089692'
