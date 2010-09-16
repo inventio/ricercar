5cfb9892b330aa013dbce902e67315d1790d56f0
lfborjas <luis.borjas@escolarea.com>
2010-09-16 12:13:32 -0600

    scores model: created migration, added file attachment validation and before_save callback

--- app/models/score.rb
+++ app/models/score.rb
@@ -1,2 +1,18 @@
+#This represents a *gist*: a self contained repository, a score can have many parts (for different instruments)
+#but lacks collaborators and is public by default
+#it can have one audio file associated with it and can be posted by an anonymous user
 class Score < ActiveRecord::Base
+  belongs_to :user
+
+  has_attached_file :sample, :url=>"#{ENV['GIT_REPOS']}/:id/attachments/:filename"
+  #according to http://filext.com/file-extension/<mp3|ogg|wav>
+  validates_attachment_content_type :sample,
+  {:content_type=>["application/ogg","audio/ogg","audio/x-ogg", "application/x-ogg",
+  #mp3:
+  "audio/mpeg", "audio/x-mpeg", "audio/mp3", "audio/x-mp3", "audio/mpeg3", "audio/x-mpeg3", "audio/mpg", "audio/x-mpg", "audio/x-mpegaudio", 
+  #wav
+  "audio/wav", "audio/x-wav", "audio/wave", "audio/x-pn-wav"],
+  :message=>"Only mp3, wav and ogg audio files are supported"
+  }
+  validates_attachment_size :sample, {:less_than=>5.megabyte}
 end
--- db/migrate/20100916054604_create_scores.rb
+++ db/migrate/20100916054604_create_scores.rb
@@ -4,7 +4,15 @@ class CreateScores < ActiveRecord::Migration
       t.string :title
       t.datetime :added
       t.boolean :private
-      t.User :author
+      t.references :user
+      #for file attachments
+      t.string :sample_file_name
+      t.string :sample_content_type
+      t.integer :sample_file_size
+      t.datetime :sample_updated_at
+
+      #for the git repo
+      t.string :repo
 
       t.timestamps
     end
