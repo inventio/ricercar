19cbcda6b0811f4d3a1c2be9b9df574030eb1d1b
lfborjas <luis.borjas@escolarea.com>
2010-09-20 20:31:09 -0600

    cleaned the repo to migrate to padrino

--- config/environment.rb
+++
@@ -1,8 +0,0 @@
-# Load the rails application
-require File.expand_path('../application', __FILE__)
-
-# Initialize the rails application
-Ricercar::Application.initialize!
-
-#configure the sass options
-Sass::Plugin.options[:style] = :compact
