07a6d80093689596bdf827851d08aac241d96e96
lfborjas <luis.borjas@escolarea.com>
2010-09-22 12:13:07 -0600

    agregada relación entre users y scores

--- app/models/score.rb
+++ app/models/score.rb
@@ -2,4 +2,11 @@ class Score
   include Mongoid::Document
   field :title, :type => String
   field :type, :type => String
+  #fields not filled in forms
+  field :last_modified, :type => DateTime, :default => lambda {DateTime.now}
+  field :git_repo, :type => String #where in the fs is the git repository for this
+  #it has an author
+  references_one :user #, :as=>"author"
+
+  #TODO: business logic: create git repo, add content, get content, get commits, etc.
 end
