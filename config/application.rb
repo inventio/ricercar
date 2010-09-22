5d6b7f2aa2afb1f3a980e7b2cb5a3c8d099a4fc6
lfborjas <luis.borjas@escolarea.com>
2010-09-22 00:14:17 -0600

    added jquery support and sass configuration for compact

--- config/application.rb
+++ config/application.rb
@@ -48,7 +48,7 @@ module Ricercar
     # config.i18n.default_locale = :de
 
     # JavaScript files you want as :defaults (application.js is always included).
-    # config.action_view.javascript_expansions[:defaults] = %w(jquery rails)
+    config.action_view.javascript_expansions[:defaults] = %w(jquery rails application)
 
     config.generators do |g|
       g.orm             :mongoid
--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,6 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 Ricercar::Application.initialize!
+
+#configure the sass plugin
+Sass::Plugin.options[:style] = :compact
